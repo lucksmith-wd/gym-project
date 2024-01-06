d0c8d928dfe245384e7e181644b4ee71c83e5456
Balazs Braun <lucksmith.wd@gmail.com>
2024-01-06 19:13:25 +0800

    Finished navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import ExerciseDetail from "./Pages/ExerciseDetail";
+import Navbar from "./components/Navbar";
+import Home from "./Pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-      <div>App</div>
-    </>
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   );
 }
 
